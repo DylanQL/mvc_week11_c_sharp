d5fe9234997c0cec64c67d9bfeed977cac16b672
DylanQL <angelo.quispe.l@tecsup.edu.pe>
2025-10-30 19:14:35 -0500

    Primera migracion exitosa

--- mvc_week11_c_sharp/Models/Category.cs
+++ mvc_week11_c_sharp/Models/Category.cs
@@ -2,7 +2,7 @@
 {
     public class Category
     {
-        public int ProductTypeId { get; set; }
+        public int CategoryId { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
 
--- mvc_week11_c_sharp/Program.cs
+++ mvc_week11_c_sharp/Program.cs
@@ -2,14 +2,15 @@ using Microsoft.EntityFrameworkCore;
 using mvc_week11_c_sharp.Models;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<StoreContex>(options =>
+    options.UseSqlServer(@"Server=DESKTOP-QRTMQJ6\SQLEXPRESS;Database=StoreDB;Integrated Security=true; trustservercertificate=True;"));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-builder.Services.AddDbContext<StoreContex>(options =>
-    options.UseSqlServer(@"Server=DESKTOP-QRTMQJ6\SQLEXPRESS;Database=StoreDB;Integrated Security=true; trustservercertificate=True;"));
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
